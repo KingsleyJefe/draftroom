7ba8314b7ed271b8b9efd02069dae0b9ff32d3d9
rafszn <yhrapha@gmail.com>
2026-02-01 21:22:58 +0100

    fix: vercel build

--- Server/api/index.ts
+++ Server/api/index.ts
@@ -1,8 +1,6 @@
 import App from "../src/app.js";
-import connectDB from "../src/db/index.js";
 
 const appInstance = new App();
 await appInstance.initialize();
-await connectDB();
 
 export default appInstance.app;
